e4e3fc372153ad57f192d231151fb1a24453d6b7
VeronicaRudiuk <veronicarudiuk@gmail.com>
2023-01-09 17:56:23 +0400

    add tabBar

--- MusicApp/ApplicationLayer/SceneDelegate.swift
+++ MusicApp/ApplicationLayer/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.overrideUserInterfaceStyle = .light
-        window?.rootViewController = ViewController()
+        window?.rootViewController = TabBarVC()
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
     }
---
+++ MusicApp/Views/TabBarVC.swift
@@ -0,0 +1,36 @@
+//
+//  TabBarVC.swift
+//  MusicApp
+//
+//  Created by Veronica Rudiuk on 09/01/2023.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        tabBar.backgroundColor = UIColor(named: K.BrandColors.tabBarBG)
+        setupVCs()
+    }
+    
+    func setupVCs() {
+        viewControllers = [
+            createNavController(for: HomeVC(), image: UIImage(named: "HomeInactive")!, selectedImage: UIImage(named: "HomeActive")!),
+            createNavController(for: FavoriteVC(), image: UIImage(named: "FavoriteInctive")!, selectedImage: UIImage(named: "FavoriteActive")!),
+            createNavController(for: PlayVC(), image: UIImage(named: "PlayInactive")!, selectedImage: UIImage(named: "PlayActive")!),
+            createNavController(for: DownloadVC(), image: UIImage(named: "DownloadInactive")!, selectedImage: UIImage(named: "DownloadActive")!),
+            createNavController(for: ProfileVC(), image: UIImage(named: "ProfileInctive")!, selectedImage: UIImage(named: "ProfileActive")!),
+        ]
+    }
+    
+    fileprivate func createNavController(for rootViewController: UIViewController, image: UIImage, selectedImage: UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.image = image.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        navController.tabBarItem.selectedImage = selectedImage.withRenderingMode(UIImage.RenderingMode.alwaysOriginal)
+        return navController
+    }
+}
+
