7978f36ceb0701935bbae16c5b274c5cb7bf9184
VeronicaRudiuk <veronicarudiuk@gmail.com>
2023-01-09 16:41:18 +0400

    add file structure, fonts, colours, images and main project settings

---
+++ MusicApp/Helpers/Constants.swift
@@ -0,0 +1,36 @@
+//
+//  Constants.swift
+//  MusicApp
+//
+//  Created by Veronica Rudiuk on 09/01/2023.
+//
+
+struct K {
+    struct BrandColors {
+        static let darkBG = "DarkBG" //#151521
+        static let grayBG = "GrayBG" //#D9D9D9, opacity 29%
+        static let tabBarBG = "TabBarBG" //#24242E
+        
+        static let blueColor = "Blue" //#24D2EA
+        static let cyanColor = "Cyan" //#08979C
+        
+        static let veryLightGray = "VeryLightGray" //#D1D5DB
+        static let lightGray = "LightGray" //#78909C
+        static let darkGray = "DarkGray" //#455A64
+        
+        static let greenGradientColor = "GreenGradientColor" //#24EBCA
+        static let purpleGradientColor = "PurpleGradientColor" //#1B0447
+    }
+    
+    struct Fonts {
+        static let poppinsSemiBold = "Poppins SemiBold"
+        static let poppinsRegular = "Poppins Regular"
+        
+        static let robotoRegular = "Roboto Regular"
+        
+        static let interExtraBold = "Inter ExtraBold"
+        static let interMedium = "Inter Medium"
+        static let interRegular = "Inter Regular"
+        static let interSemiBold = "Inter SemiBold"
+    }
+}
---
+++ MusicApp/Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Veronica Rudiuk on 09/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "HGFFD"
+        view.backgroundColor = .red
+    }
+
+
+}
+
