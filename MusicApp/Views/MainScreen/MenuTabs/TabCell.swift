9277c66a57fd233258cbecb87a92e9302d32c0e8
VeronicaRudiuk <veronicarudiuk@gmail.com>
2023-01-19 16:16:02 +0400

    add interim changes in main screen

--- MusicApp/Views/MainScreen/MenuTabs/TabCell.swift
+++ MusicApp/Views/MainScreen/MenuTabs/TabCell.swift
@@ -8,18 +8,18 @@
 import UIKit
 
 class TabCell: UICollectionViewCell {
-  private var tabSV: UIStackView!
+  private var tabSV = UIStackView()
 
   var tabTitle: UILabel!
   var indicatorView: UIView!
-  var indicatorColor: UIColor = .green
+    var indicatorColor = UIColor(named: K.BrandColors.blueColor)
 
   override var isSelected: Bool {
     didSet {
       DispatchQueue.main.async {
         UIView.animate(withDuration: 0.2) {
           self.indicatorView.backgroundColor = self.isSelected ? self.indicatorColor : UIColor.clear
-          self.tabTitle.textColor = self.isSelected ? .green : .white
+            self.tabTitle.textColor = self.isSelected ? UIColor(named: K.BrandColors.blueColor) : .white
           self.layoutIfNeeded()
         }
       }
@@ -37,8 +37,8 @@ class TabCell: UICollectionViewCell {
     super.init(frame: frame)
     tabSV = UIStackView()
     tabSV.axis = .horizontal
-    tabSV.distribution = .equalCentering
-    tabSV.alignment = .center
+//    tabSV.distribution = .fillEqually
+//    tabSV.alignment = .center
     tabSV.spacing = 10.0
     addSubview(tabSV)
 
@@ -47,8 +47,6 @@ class TabCell: UICollectionViewCell {
     tabTitle = UILabel()
     tabTitle.textAlignment = .center
     self.tabSV.addArrangedSubview(tabTitle)
-
-
 
     // TabSv Constraints
     tabSV.translatesAutoresizingMaskIntoConstraints = false
@@ -81,7 +79,7 @@ class TabCell: UICollectionViewCell {
       indicatorView.heightAnchor.constraint(equalToConstant: 2),
       indicatorWidth,
       indicatorView.centerXAnchor.constraint(equalTo: tabTitle.centerXAnchor),
-      indicatorView.bottomAnchor.constraint(equalTo: self.tabTitle.bottomAnchor, constant: 10)
+      indicatorView.topAnchor.constraint(equalTo: tabTitle.bottomAnchor, constant: 10)
     ])
   }
 }
